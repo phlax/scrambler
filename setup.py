8a597e50ae69458999a74cf625f71db6d262c80f
Ryan Northey <ryan@space23.net>
2015-05-17 12:04:25 +0000

    readying setup.py for initial release

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ from setuptools import setup, find_packages
 from scramble import __version__ as version
 
 
-install_requires = [
-    'setuptools']
+install_requires = ['setuptools']
 
 # collective.recipe.omelette is only installed as it provides a
 # good example of a namespaced package
@@ -16,8 +15,14 @@ tests_require = install_requires + ['collective.recipe.omelette']
 setup(
     name='scramble',
     version=version,
-    description="Symlink python modules into a folder",
+    description="Symlink (namespaced) python packages into a single folder to aid development",
     classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python 2.6",
+        "Programming Language :: Python 2.7",
+        "Programming Language :: Python 3.2",
+        "Programming Language :: Python 3.3",
         "Programming Language :: Python 3.4",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
